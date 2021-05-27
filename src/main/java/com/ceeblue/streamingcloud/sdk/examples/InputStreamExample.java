9ef0a5d58ee98d35bcc2389594a4c47fffb66727
sugo <sugovladik@gmail.com>
2021-05-27 10:31:51 +0300

    Set current latest version to gradle dependencies. Add streamingcloud to package name

--- src/main/java/com/ceeblue/streamingcloud/sdk/examples/InputStreamExample.java
+++ src/main/java/com/ceeblue/streamingcloud/sdk/examples/InputStreamExample.java
@@ -1,33 +1,32 @@
-package org.example.javasdkexamples.inputStream;
+package com.ceeblue.streamingcloud.sdk.examples;
 
-import com.ceeblue.sdk.authentiffication.AuthenticationClient;
-import com.ceeblue.sdk.authentiffication.AuthenticationClientImplementation;
-import com.ceeblue.sdk.http.HttpClient;
-import com.ceeblue.sdk.http.template.HttpRestTemplate;
-import com.ceeblue.sdk.http.template.utils.RestTemplateResponseErrorHandler;
-import com.ceeblue.sdk.settings.Credential;
-import com.ceeblue.sdk.streams.input.InputApiClientImplementation;
-import com.ceeblue.sdk.streams.input.InputStreamClient;
-import com.ceeblue.sdk.streams.input.StreamBuilder;
-import com.ceeblue.sdk.streams.input.models.Access;
-import com.ceeblue.sdk.streams.input.models.InputFormat;
-import com.ceeblue.sdk.streams.input.models.OutputSettings;
-import com.ceeblue.sdk.streams.input.models.SpeedPreset;
-import com.ceeblue.sdk.streams.input.models.encoder.EncoderSettings;
-import com.ceeblue.sdk.streams.input.models.encoder.H264Settings;
-import com.ceeblue.sdk.streams.input.models.inputs.CreatedInput;
-import com.ceeblue.sdk.streams.input.models.inputs.Input;
-import com.ceeblue.sdk.streams.input.models.tracks.AudioTrack;
-import com.ceeblue.sdk.streams.input.models.tracks.VideoTrack;
-import com.ceeblue.sdk.utils.ClientException;
+import com.ceeblue.streamingcloud.sdk.authentiffication.AuthenticationClient;
+import com.ceeblue.streamingcloud.sdk.authentiffication.AuthenticationClientImplementation;
+import com.ceeblue.streamingcloud.sdk.http.HttpClient;
+import com.ceeblue.streamingcloud.sdk.http.template.HttpRestTemplate;
+import com.ceeblue.streamingcloud.sdk.http.template.utils.RestTemplateResponseErrorHandler;
+import com.ceeblue.streamingcloud.sdk.settings.Credential;
+import com.ceeblue.streamingcloud.sdk.streams.input.InputApiClientImplementation;
+import com.ceeblue.streamingcloud.sdk.streams.input.InputStreamClient;
+import com.ceeblue.streamingcloud.sdk.streams.input.models.encoder.EncoderSettings;
+import com.ceeblue.streamingcloud.sdk.streams.input.models.encoder.H264Settings;
+import com.ceeblue.streamingcloud.sdk.streams.input.models.tracks.AudioTrack;
+import com.ceeblue.streamingcloud.sdk.streams.input.models.tracks.TrackType;
+import com.ceeblue.streamingcloud.sdk.streams.input.models.tracks.VideoTrack;
+import com.ceeblue.streamingcloud.sdk.utils.ClientException;
+import com.ceeblue.streamingcloud.sdk.streams.input.StreamBuilder;
+import com.ceeblue.streamingcloud.sdk.streams.input.models.Access;
+import com.ceeblue.streamingcloud.sdk.streams.input.models.InputFormat;
+import com.ceeblue.streamingcloud.sdk.streams.input.models.OutputSettings;
+import com.ceeblue.streamingcloud.sdk.streams.input.models.SpeedPreset;
+import com.ceeblue.streamingcloud.sdk.streams.input.models.inputs.CreatedInput;
+import com.ceeblue.streamingcloud.sdk.streams.input.models.inputs.Input;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 
-import static com.ceeblue.sdk.streams.input.models.CodecName.AAC;
-import static com.ceeblue.sdk.streams.input.models.CodecName.H264;
-import static com.ceeblue.sdk.streams.input.models.tracks.TrackType.Audio;
-import static com.ceeblue.sdk.streams.input.models.tracks.TrackType.Video;
+import static com.ceeblue.streamingcloud.sdk.streams.input.models.CodecName.AAC;
+import static com.ceeblue.streamingcloud.sdk.streams.input.models.CodecName.H264;
 
 public class InputStreamExample {
 
@@ -49,8 +48,8 @@ public class InputStreamExample {
                         new OutputSettings(true)
                                 .version(1)
                                 .tracks(Arrays.asList(
-                                        new VideoTrack(Video, new H264Settings(H264, 110, SpeedPreset.faster, 20)),
-                                        new AudioTrack(Audio, new EncoderSettings(AAC, 30))
+                                        new VideoTrack(TrackType.Video, new H264Settings(H264, 110, SpeedPreset.faster, 20)),
+                                        new AudioTrack(TrackType.Audio, new EncoderSettings(AAC, 30))
                                 )));
 
         Input input = builder.build();
