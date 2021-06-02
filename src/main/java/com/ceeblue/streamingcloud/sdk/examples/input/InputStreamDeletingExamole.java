31c094e2cb283e49b508bfdb2fd4e53ff8df9fab
sugo <sugovladik@gmail.com>
2021-06-02 14:48:18 +0300

    Replace certain stream id to help text

--- src/main/java/com/ceeblue/streamingcloud/sdk/examples/input/InputStreamDeletingExamole.java
+++ src/main/java/com/ceeblue/streamingcloud/sdk/examples/input/InputStreamDeletingExamole.java
@@ -4,12 +4,14 @@ import com.ceeblue.streamingcloud.sdk.examples.apiclients.ApiClientsCreationExam
 import com.ceeblue.streamingcloud.sdk.streams.input.InputStreamClient;
 
 public class InputStreamDeletingExamole {
+
     public static void main(String[] args) {
-        String streamId = "0b8d2ce2-06ca-446d-99e9-e067fdfdf8b3";
+        String streamId = "Place your input id here";
 
         InputStreamClient inputStreamClient = ApiClientsCreationExamples.getInputStreamClient();
 
         inputStreamClient.deleteInput(streamId);
 
     }
+
 }
