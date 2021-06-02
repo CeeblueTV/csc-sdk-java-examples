1f6b6af5a714e06568314ac78bfaf0c6c1b7922c
sugo <sugovladik@gmail.com>
2021-06-02 16:16:27 +0300

    Fix input stream example

--- src/main/java/com/ceeblue/streamingcloud/sdk/examples/input/InputStreamCreationExample.java
+++ src/main/java/com/ceeblue/streamingcloud/sdk/examples/input/InputStreamCreationExample.java
@@ -2,41 +2,29 @@ package com.ceeblue.streamingcloud.sdk.examples.input;
 
 import com.ceeblue.streamingcloud.sdk.examples.apiclients.ApiClientsCreationExamples;
 import com.ceeblue.streamingcloud.sdk.streams.input.InputStreamClient;
-import com.ceeblue.streamingcloud.sdk.streams.input.StreamBuilder;
-import com.ceeblue.streamingcloud.sdk.streams.models.InputFormat;
 import com.ceeblue.streamingcloud.sdk.streams.input.models.OutputSettings;
-import com.ceeblue.streamingcloud.sdk.streams.input.models.SpeedPreset;
-import com.ceeblue.streamingcloud.sdk.streams.input.models.encoder.EncoderSettings;
-import com.ceeblue.streamingcloud.sdk.streams.input.models.encoder.H264Settings;
 import com.ceeblue.streamingcloud.sdk.streams.input.models.inputs.CreatedInput;
 import com.ceeblue.streamingcloud.sdk.streams.input.models.inputs.Input;
-import com.ceeblue.streamingcloud.sdk.streams.input.models.tracks.AudioTrack;
-import com.ceeblue.streamingcloud.sdk.streams.input.models.tracks.TrackType;
-import com.ceeblue.streamingcloud.sdk.streams.input.models.tracks.VideoTrack;
+import com.ceeblue.streamingcloud.sdk.streams.models.InputFormat;
 
-import java.util.Arrays;
-
-import static com.ceeblue.streamingcloud.sdk.streams.input.models.CodecName.AAC;
-import static com.ceeblue.streamingcloud.sdk.streams.input.models.CodecName.H264;
+import static com.ceeblue.streamingcloud.sdk.streams.input.utils.Presets.H264_1080p_30fps_4Mbps;
+import static com.ceeblue.streamingcloud.sdk.streams.input.utils.Presets.Opus_48kHz_2Ch_120Kbps;
 
 public class InputStreamCreationExample {
 
     public static void main(String[] args) {
         InputStreamClient inputStreamClient = ApiClientsCreationExamples.getInputStreamClient();
 
-        StreamBuilder builder = new StreamBuilder(InputFormat.WebRTC)
+        Input input = new Input(InputFormat.WebRTC)
                 .setOutput(
-                        new OutputSettings(true)
-                                .version(1)
-                                .tracks(Arrays.asList(
-                                        new VideoTrack(TrackType.Video, new H264Settings(H264, 110, SpeedPreset.faster, 20)),
-                                        new AudioTrack(TrackType.Audio, new EncoderSettings(AAC, 30))
-                                )));
-
-        Input input = builder.build();
+                        new OutputSettings()
+                                .addTrack(H264_1080p_30fps_4Mbps())
+                                .addTrack(Opus_48kHz_2Ch_120Kbps())
+                );
 
         CreatedInput createdInput = inputStreamClient.createInput(input);
 
         System.out.println(createdInput);
     }
+
 }
