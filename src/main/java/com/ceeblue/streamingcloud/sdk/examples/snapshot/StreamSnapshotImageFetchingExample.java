8786ca9f8b3ede08c902cf4b48f8d4f3b7a49821
sugo <sugovladik@gmail.com>
2021-06-18 16:57:33 +0300

    Move image creation from api to example

--- src/main/java/com/ceeblue/streamingcloud/sdk/examples/snapshot/StreamSnapshotImageFetchingExample.java
+++ src/main/java/com/ceeblue/streamingcloud/sdk/examples/snapshot/StreamSnapshotImageFetchingExample.java
@@ -5,8 +5,21 @@ import com.ceeblue.streamingcloud.sdk.examples.apiclients.ApiClientCreateExample
 import com.ceeblue.streamingcloud.sdk.streams.exceptions.ClientException;
 import com.ceeblue.streamingcloud.sdk.streams.models.Source;
 import com.ceeblue.streamingcloud.sdk.streams.snapshot.SnapshotClient;
+import com.ceeblue.streamingcloud.sdk.streams.snapshot.models.ContentType;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
 public class StreamSnapshotImageFetchingExample {
+
+    private static final String SNAPSHOT = "snapshot.";
+
+    private static final String CONTENT_TYPE = "Content-Type";
 
     public static void main(String[] args) {
         SnapshotClient snapshotClient = ApiClientCreateExamples
@@ -15,10 +28,20 @@ public class StreamSnapshotImageFetchingExample {
         String streamId = "Place your input streamId here";
 
         try {
-            var image = snapshotClient.getSnapshotImage(streamId, Source.Incoming);
+            var responseInfo = snapshotClient.getSnapshotImage(streamId, Source.Outgoing);
 
-            System.out.println("Snapshot location"+image.getAbsolutePath());
-        } catch (ClientException exception) {
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(responseInfo.getBody());
+            BufferedImage bufferedImage = ImageIO.read(inputStream);
+            String imageFormat = Arrays.stream(ContentType.values())
+                    .filter(e -> e.contentType.equals(responseInfo.getHeaders().get(CONTENT_TYPE)))
+                    .findFirst()
+                    .orElseThrow(() -> new ClientException("Unknown type: " + responseInfo.getHeaders().get(CONTENT_TYPE))).format;
+
+            File snapshot = new File(LocalDateTime.now() + SNAPSHOT + imageFormat);
+            ImageIO.write(bufferedImage, imageFormat, snapshot);
+
+            System.out.println("Snapshot location" + snapshot.getAbsolutePath());
+        } catch (ClientException | IOException exception) {
             System.err.println("Something went wrong: " + exception);
         }
     }
